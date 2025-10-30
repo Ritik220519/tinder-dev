e2b3e63cf6f97f12cc3f379d32ca174818c3a9e3
Ritik220519 <ritik220519@gmail.com>
2025-10-30 15:53:43 +0530

    made test api call

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ const app = express();
 app.use("/hello" , (req, res)=>{
     res.send("Hello from Ritik");
 })
+
+app.use("/user" , (req,res)=>{
+    res.send("Hello from the user")
+})
 app.use("/" , (req, res)=>{
     res.send("Hello from backend");
-})
+});
 
 app.listen(3528 , ()=>{
     console.log("server is running on port 3528")
